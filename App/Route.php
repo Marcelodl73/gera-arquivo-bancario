6157c10e67a5b99b42940ec213a6c04092d0147b
Marcelo da Luz <marcelodl73@gmail.com>
2019-08-15 10:56:47 -0300

    aplicação para gerar arquivo bancario

---
+++ App/Route.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App;
+
+use MF\Init\Bootstrap;
+
+class Route extends Bootstrap {
+
+	protected function initRoutes() {
+
+		$routes['home'] = array(
+			'route' => '/',
+			'controller' => 'indexController',
+			'action' => 'index'
+		);
+
+		$routes['cadastrarse'] = array(
+			'route' => '/cadastrarse',
+			'controller' => 'indexController',
+			'action' => 'cadastrarse'
+		);
+
+		$routes['registrar'] = array(
+			'route' => '/registrar',
+			'controller' => 'indexController',
+			'action' => 'registrar'
+		);
+
+		$routes['autenticar'] = array(
+			'route' => '/autenticar',
+			'controller' => 'AuthController',
+			'action' => 'autenticar'
+		);
+
+		$routes['timeline'] = array(
+			'route' => '/timeline',
+			'controller' => 'AppController',
+			'action' => 'timeline'
+		);
+
+		$routes['sair'] = array(
+			'route' => '/sair',
+			'controller' => 'AuthController',
+			'action' => 'sair'
+		);
+
+		$routes['gerar'] = array(
+			'route' => '/gerar',
+			'controller' => 'AuthController',
+			'action' => 'gerar'
+		);
+		
+
+		$this->setRoutes($routes);
+	}
+
+}
+
+?>
